39e819fdb0f5e0890509ec92187bb0ff7a1b2850
Isha Bule <ishabule753@gmail.com>
2023-08-05 18:51:28 +0530

    Dashboard added

--- future/src/pages/DashBoard.js
+++ future/src/pages/DashBoard.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box, Flex, Input, InputGroup, InputRightElement, IconButton, Stack, Text } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { Card, CardHeader, Image, Avatar, Heading, Button, CardFooter } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export default function DashBoard() {
+    const [careersData, setCareersData] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:8082/api/career')
+            .then((response) => response.json())
+            .then((data) => {
+                // Set the fetched data to the state
+                setCareersData(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }, []); // Empty dependency array ensures useEffect runs only once on mount
+
+    const handleSearchChange = (event) => {
+        setSearchValue(event.target.value);
+
+    };
+
     return (
-        <div>DashBoard</div>
-    )
+        <Box p={4} width="100%">
+            <Flex align="center" justify="center" direction="column">
+                {/* Search Box */}
+                <Box mb={4} width="70%">
+                    <InputGroup>
+                        <Input placeholder="Search" value={searchValue} onChange={handleSearchChange} />
+                        <InputRightElement>
+                            <Link to={`/major/${searchValue}`}>
+                                <IconButton icon={<SearchIcon />} />
+                            </Link>
+                        </InputRightElement>
+                    </InputGroup>
+                </Box>
+
+                <Box width="75%" p={5}>
+                    <Stack spacing={4}>
+                        <Flex align="center" justify="center">
+                            {careersData.map((career, index) => (
+                                <Card key={index} m={'5'} width="400px" align='center'>
+                                    <CardHeader>
+                                        <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+                                            <Box>
+                                                <Heading size="md">{career.name}</Heading>
+                                            </Box>
+                                        </Flex>
+                                    </CardHeader>
+
+                                    <Image height="300px" src={career.image} alt="Career Image" />
+
+                                    <CardFooter justify="space-between" flexWrap="wrap">
+                                        {/* Use the searchValue in the to attribute of the Link */}
+                                        <Button flex="1" variant="ghost">
+                                            <Link to={`/major/${career.name}`}> Explore </Link>
+                                        </Button>
+                                    </CardFooter>
+                                </Card>
+                            ))}
+                        </Flex>
+                    </Stack>
+                </Box>
+            </Flex>
+        </Box>
+    );
 }
--- future/src/pages/Major.js
+++ future/src/pages/Major.js
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Heading, Text, Button } from '@chakra-ui/react';
+import { Box, Flex, Stack, Image } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
 
 export default function Major() {
+    const { major } = useParams();
+    const [majorsData, setmajorsData] = useState([]);
+    console.log(major);
+    const capMajor = major.charAt(0).toUpperCase() + major.slice(1);
+
+    useEffect(() => {
+        fetch(`http://localhost:8082/api/career/${capMajor}`) // Remove the colon from here
+            .then((response) => response.json())
+            .then((data) => {
+                // Set the fetched data to the state
+                setmajorsData(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }, []); // Empty dependency array ensures useEffect runs only once on mount
+
+    // console.log(majorsData);
     return (
-        <div>Major</div>
-    )
+        <Card align='center'>
+            <CardHeader>
+                <Heading size='md'> Explore {capMajor}</Heading>
+            </CardHeader>
+            <CardBody>
+                <Text>{majorsData.description}</Text>
+
+                <Box>
+                    <Stack spacing={4}>
+                        <Flex align="center" justify="center">
+                            {majorsData.subFields.map((subfield, index) => (
+                                <Card key={index} m={'5'} width="300px" align='center'>
+                                    <CardHeader>
+                                        <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+                                            <Box>
+                                                <Heading size="md">{subfield.name}</Heading>
+                                            </Box>
+                                        </Flex>
+                                    </CardHeader>
+
+                                    <Image height="300px" src={subfield.image} alt="subfield Image" />
+
+                                    <CardFooter justify="space-between" flexWrap="wrap">
+                                        {/* Use the searchValue in the to attribute of the Link */}
+                                        <Button flex="1" variant="ghost">
+                                            <Link to={`/major/${subfield.name}`}> Explore </Link>
+                                        </Button>
+                                    </CardFooter>
+                                </Card>
+                            ))}
+                        </Flex>
+                    </Stack>
+                </Box>
+
+
+            </CardBody>
+
+        </Card>
+    );
 }
